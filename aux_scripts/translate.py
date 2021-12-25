e0e013dc7808835b98afd7e3243f946f86105275
Adi <adi.album1@gmail.com>
2021-12-25 11:29:54 +0200

    update folder name

---
+++ aux_scripts/translate.py
@@ -0,0 +1,16 @@
+from .. import params
+
+def coordinates_to_pixels(x, y):
+    row = y
+    col = x
+
+    # translate
+    row += params.axis_range
+    col += params.axis_range
+
+    # scale
+
+    row = round(row * params.im_height / (2 * params.axis_range))
+    col = round(col * params.im_width / (2 * params.axis_range))
+
+    return (row, col)
