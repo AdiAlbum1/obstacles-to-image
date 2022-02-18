152138408d7dd0f707a1ba52800f42cd129c0f92
Adi <adi.album1@gmail.com>
2022-02-18 14:18:08 +0200

    remove commented out code

--- params.py
+++ params.py
@@ -8,8 +8,6 @@ max_index_base_obstacle_maps = 55
 max_num_additional_obstacles = 29
 
 lambda_scale = 0.33
-# lambda_scale = 1.0
-# lambda_scale = 3.0
 
 # Training parameters
 batch_size = 50
@@ -17,5 +15,4 @@ num_batches_in_epoch = 20000
 num_test_set_batches = 30
 
 # Inference parameters
-# network_path = ".\\models\\current_best\\test_model.pt"
-network_path = "test_model.pt"
+network_path = ".\\models\\current_best\\test_model.pt"
